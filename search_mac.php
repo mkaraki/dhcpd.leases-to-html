415d79060a717e9f5aec74d6118ffaa60b7efd22
mkaraki <mkaraki_apps_publisher@outlook.jp>
2018-05-30 06:20:14 +0900

    support UTC in mac search

--- search_mac.php
+++ search_mac.php
@@ -1,6 +1,11 @@
 <?php
     require_once(dirname(__FILE__)."/perse.php");
 
+    $res;
+
+    $pinfo_time = "";
+    if ($src_info['si']['si']['isutc']) $pinfo_time=$pinfo_time." (UTC)";
+
     function mac_toq($mc_o){
         if ($mc_o == ""){return "";}
         $mc=str_replace(array(':','%3A'),'',$mc_o);
@@ -9,20 +14,13 @@
         return $mc;
     }
 
-    $res;
-
-    function S_query($mac){
-        global $res;
-        return get_per_mac($mac);
-    }
-
     function query($targ){
         global $res;
         if (!isset($res)){return "None";}
         return $res[$targ];
     }
 
-    if (isset($_GET['mac'])){$res=S_query(mac_toq($_GET['mac']));}
+    if (isset($_GET['mac'])){$res=get_per_mac(mac_toq($_GET['mac']));}
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,8 +36,8 @@
             <tbody>
                 <tr><td>Mac Address</td><td>:</td><td><?php echo query("mac"); ?></td></tr>
                 <tr><td>IP Address</td><td>:</td><td><?php echo query("ip"); ?></td></tr>
-                <tr><td>Address Entry</td><td>:</td><td><?php echo query("end"); ?></td></tr>
-                <tr><td>Address Expiration</td><td>:</td><td><?php echo query("start"); ?></td></tr>
+                <tr><td>Address Entry<?php echo $pinfo_time ?></td><td>:</td><td><?php echo query("end"); ?></td></tr>
+                <tr><td>Address Expiration<?php echo $pinfo_time ?></td><td>:</td><td><?php echo query("start"); ?></td></tr>
             </tbody>
         </table>
     </body>
